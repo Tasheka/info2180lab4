6faf2623730f16cd7817e0d859eb0b38a93fd3fa
Tasheka Martin <tasheka_m@yahoo.com>
2017-10-20 18:21:27 +0000

    Exercise 1

--- maze.js
+++ maze.js
@@ -1,1 +1,11 @@
-//code here
+window.onload = function () {
+	  main();
+	};
+	
+
+	function main() 
+	{
+		var boun1redchange = document.getElementById("boundary1");
+		boun1redchange.onmouseover = function() {
+			this.setAttribute("outcome", "youlose");
+		};
