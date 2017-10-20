7a0324324bced194ae5a6fe874477ecfaaf409a0
Tasheka Martin <tasheka_m@yahoo.com>
2017-10-20 21:59:04 +0000

    Exercise 2

--- maze.js
+++ maze.js
@@ -8,9 +8,26 @@ window.onload = function ()
 	{
 		//Exercise 1: Single Boundary Turns Red
 		
-		var boun1redchange = document.getElementById("boundary1");
+		var boun1ToRedChange = document.getElementById("boundary1");
 		boun1redchange.onmouseover = function() 
 		{
 			this.setAttribute("outcome", "youlose");
 		};
+		
+		//Exercise 2: All boundaries glow red on hover
+		
+		var allBounToChangeRed = document.querySelectorAll(".boundary");
+	//	var e = 0;
+	//	var h = 0;
+		var m = 0;
+			for (var e = 0; e < boundary.length; e++)
+			{
+				boundary[e].onmouseover = function()
+				{
+					for (var h = 0; h< boundary.length-1;h++)
+					{
+						boundary[h].setAttribute("class","boundary youlose");
+					}
+				}
+			}
 	}
