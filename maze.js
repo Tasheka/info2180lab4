d9ffc8edbf8e6f882516afaa7a9d80831be91018
Tasheka Martin <tasheka_m@yahoo.com>
2017-10-20 22:27:43 +0000

    Exercise 3

--- maze.js
+++ maze.js
@@ -8,26 +8,41 @@ window.onload = function ()
 	{
 		//Exercise 1: Single Boundary Turns Red
 		
-		var boun1ToRedChange = document.getElementById("boundary1");
-		boun1redchange.onmouseover = function() 
+		var boun1toredchange = document.getElementById("boundary1");
+		boun1toredchange.onmouseover = function() 
 		{
 			this.setAttribute("outcome", "youlose");
 		};
 		
 		//Exercise 2: All boundaries glow red on hover
 		
-		var allBounToChangeRed = document.querySelectorAll(".boundary");
+		var allbounTochangered = document.querySelectorAll(".boundary");
 	//	var e = 0;
 	//	var h = 0;
 		var m = 0;
-			for (var e = 0; e < boundary.length; e++)
+			for (var e = 0; e < allbounTochangered.length; e++)
 			{
-				boundary[e].onmouseover = function()
+				allbounTochangered[e].onmouseover = function()
 				{
-					for (var h = 0; h< boundary.length-1;h++)
+					for (var h = 0; h< allbounTochangered.length-1;h++)
 					{
-						boundary[h].setAttribute("class","boundary youlose");
+						allbounTochangered[h].setAttribute("class","boundary youlose");
 					}
-				}
+						m++;
+						document.getElementById("status").innerHTML = "You Lose. Better Luck next Time.";
+				};
+			}	
+			
+			//Exercise 3: Alerts on successful completion of maze
+			
+		var win = document.getElementById("end");
+		win.onmouseover = function()
+		{
+			if (m == 0)
+			{
+				window.alert("You Win");
+				document.getElementById("status").innerHTML = "Congratulations!! You Win";
 			}
+		};
+		
 	}
